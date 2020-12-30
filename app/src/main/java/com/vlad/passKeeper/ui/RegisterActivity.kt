b5259c124dc3cb6485f6b1375feebf3888895d39
Vlad Adrian <vladadrian1361@gmail.com>
2020-12-30 19:21:32 +0200

    Changed theme for 2 activities

--- app/src/main/java/com/vlad/passKeeper/ui/RegisterActivity.kt
+++ app/src/main/java/com/vlad/passKeeper/ui/RegisterActivity.kt
@@ -15,7 +15,8 @@ class RegisterActivity : AppCompatActivity() {
     private lateinit var password: TextView
     private lateinit var rPassword: TextView
     private lateinit var auth: FirebaseAuth
-    val PASSWORD_PATTERN: String = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{4,}$"
+    val PASSWORD_PATTERN: String =
+        "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{4,}$"
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,29 +31,41 @@ class RegisterActivity : AppCompatActivity() {
 
         val createAccountButton = findViewById<TextView>(R.id.createAccountButton)
 
-        createAccountButton.setOnClickListener { createAccount(email.text.toString(), password.text.toString(), rPassword.text.toString()) }
+        createAccountButton.setOnClickListener {
+            createAccount(
+                email.text.toString(),
+                password.text.toString(),
+                rPassword.text.toString()
+            )
+        }
     }
 
     private fun createAccount(email: String, pass: String, rPass: String) {
         val pattern = Pattern.compile(PASSWORD_PATTERN)
         val matcher = pattern.matcher(pass)
-        if(email.isEmpty() || pass.isEmpty() || rPass.isEmpty()){
-            Toast.makeText(this, "PLease complete all fields!",Toast.LENGTH_SHORT).show()
-        }else if(!pass.equals(rPass)){
-            Toast.makeText(this, "Passwords must be the same!",Toast.LENGTH_SHORT).show()
-        }else if(!matcher.matches()){
-            Toast.makeText(this, "Password must contain at least one special character, one digit, one uppercase letter",Toast.LENGTH_LONG).show()
-        }else
+        if (email.isEmpty() || pass.isEmpty() || rPass.isEmpty()) {
+            Toast.makeText(this, "PLease complete all fields!", Toast.LENGTH_SHORT).show()
+        } else if (!pass.equals(rPass)) {
+            Toast.makeText(this, "Passwords must be the same!", Toast.LENGTH_SHORT).show()
+        } else if (!matcher.matches()) {
+            Toast.makeText(
+                this,
+                "Password must contain at least one special character, one digit, one uppercase letter",
+                Toast.LENGTH_LONG
+            ).show()
+        } else
             auth.createUserWithEmailAndPassword(email, pass)
-                .addOnCompleteListener(this){task ->
+                .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
                         Log.d("signupAttemtpt", "createUserWithEmail:success")
-                        Toast.makeText(baseContext, "Registration successful.", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(baseContext, "Registration successful.", Toast.LENGTH_SHORT)
+                            .show()
                         startActivity(Intent(this, LoginActivity::class.java))
                         this.finish()
                     } else {
                         Log.w("signupAttemtpt", "createUserWithEmail:failure", task.exception)
-                        Toast.makeText(baseContext, "Registration failed.", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(baseContext, "Registration failed.", Toast.LENGTH_SHORT)
+                            .show()
 
                     }
                 }
